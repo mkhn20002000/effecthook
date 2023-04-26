74a08c2ce380650e7464153b46232aeaa4e0a933
Manoochehr Khatami <manouchehr.khatami@gmail.com>
2023-04-26 11:09:27 +0200

    commit 1.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 import reactLogo from "./assets/react.svg";
 import "./App.css";
+import { useEffect, useRef } from "react";
 
 function App() {
-  return <div>Applictaion</div>;
+  const ref = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (ref.current) ref.current.focus();
+  });
+
+  return (
+    <div>
+      <input ref={ref} type="text" className="form-control" />
+    </div>
+  );
 }
 
 export default App;
